a84213a7bf7260d3bffd19f60f94eb9cfc8440f9
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-23 09:09:55 -0700

    Completed exercise 8

--- ex08.js
+++ ex08.js
@@ -1,7 +1,16 @@
 // Exercise 8: Write a function named 'longestWord' that takes a string of text and returns the longest word in the string.
 
 function longestWord(text) {
-  // your code here
+  const textArray = text.split(' ')
+  let longestWord = ''
+
+  for  (let i = 0; i < textArray.length; i++){
+    const currentWord = textArray[i]
+    if (currentWord.length > longestWord.length){
+      longestWord = currentWord
+    }
+  }
+  return longestWord
 }
 
-console.error.longestWord('The quick brown fox jumps over the lazy dog') // 'jumps'
+console.log(longestWord('Stratigically super-long wording utilized along sentences'))
