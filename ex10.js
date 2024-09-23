e041a37ca019850b19395e4ff8fc6e2136497a13
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-23 11:06:36 -0700

    Completed exercise 10

--- ex10.js
+++ ex10.js
@@ -1,20 +1,11 @@
 // Exercise 10: Write a function named 'formatDate' that takes a date string (in the format 'YYYY-MM-DD') and returns a formatted date string (e.g., 'January 1, 2022').
 
-// function formatDate(dateStr) {
-//   const dateArray = dateStr.split("-")
-
-//   let monthDate = ""
-//   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemer", "October", "November", "December"]
-
-//   for (let i = 0; i < months.length; i++) {
-//     const currentMonth = months[i]
-//     if (dateArray[1] == currentMonth) {
-//       monthDate = currentMonth
-//     }
-//   }
-//   return monthDate
-// }
+function formatDate(dateStr) {
+  const dateArray = dateStr.split("-")
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemer", "October", "November", "December"]
+  const currentMonth = months[parseInt(dateArray[1]) - 1]
+  return `Today is: ${currentMonth} ${dateArray[2]}, ${dateArray[0]}.`
+}
 
 
-
-// console.log(formatDate('2022-01-01')) // 'January 1, 2022'
+console.log(formatDate('2024-09-23')) // 'January 1, 2022'
