9f5dbac4fb9a42a17ab6121f138fb85efbf80e9e
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-25 22:13:40 -0700

    Completed exercise 4

--- ex04.js
+++ ex04.js
@@ -1,7 +1,10 @@
 // Exercise 4: Write a function named 'countOccurrences' that takes an array of strings and returns an object with the count of each string in the array.
 
 function countOccurrences(arr) {
-  // your code here
+  return arr.reduce((product, current) => {
+    product[current] = (product[current] || 0) + 1
+    return product
+  }, {})
 }
 
 console.log(countOccurrences(['apple', 'banana', 'apple'])) // { apple: 2, banana: 1 }
